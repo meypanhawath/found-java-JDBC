90e2c278ed269cc5323715c7081f5e4c15cde2e9
Raz <panhawath18@gmail.com>
2025-07-30 02:55:43 +0700

    update: custom table for output database in java console

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -2,10 +2,7 @@ package bank;
 
 import bank.model.Customer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.UUID;
 
 public class Main {
@@ -14,10 +11,10 @@ public class Main {
 
         Customer customer = new Customer(
                 UUID.randomUUID().toString(),
-                "Mey Panhawath",
+                "KangKang",
                 "Male",
-                "panhawath18@gmail.com",
-                "078654234",
+                "kangkang@gmail.com",
+                "041987662",
                 false);
 
             String url = "jdbc:postgresql://localhost:5432/aba_bank_db";
@@ -39,10 +36,35 @@ public class Main {
             stmt.setString(5, customer.getPhone());
             stmt.setBoolean(6, customer.getDeleted());
             int updateInserted = stmt.executeUpdate();
-            System.out.printf("Update: %d successfully.", updateInserted);
+            System.out.printf("Update: %d successfully.\n\n", updateInserted);
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
 
+
+        String selectSQL = "SELECT * FROM customer";
+        try(Connection connection = DriverManager.getConnection(url, user, password)){
+
+            PreparedStatement stmt = connection.prepareStatement(selectSQL);
+            ResultSet resultSet = stmt.executeQuery();
+
+            System.out.println("=".repeat(130));
+            System.out.printf("| %-40s | %-15s | %-6s | %-25s | %-12s | %-8s |\n", "UUID", "full_name", "gender", "email", "phone", "is_deleted");
+            System.out.println("=".repeat(130));
+
+            while (resultSet.next()){
+                String uuid = resultSet.getString("uuid");
+                String fullName = resultSet.getString("full_name");
+                String gender = resultSet.getString("gender");
+                String email = resultSet.getString("email");
+                String phone = resultSet.getString("phone");
+                Boolean isDeleted = resultSet.getBoolean("is_deleted");
+
+                System.out.printf("| %-40s | %-15s | %-6s | %-25s | %-12s | %-8s |\n", uuid, fullName, gender, email, phone, isDeleted ? "Yes" : "No");
+            }
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
